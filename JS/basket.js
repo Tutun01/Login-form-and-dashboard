f14966fe055654da4ed5099b918c2c69e42cf847
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2026-01-05 08:13:35 +0100

    * JS for deleted and bought items

---
+++ JS/basket.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const totalEl = document.getElementById('grandTotal');
+    const buyBtn = document.querySelector('.buyBtn');
+
+    function recalculateTotal() {
+        let sum = 0;
+
+        document.querySelectorAll('.basket-item').forEach(item => {
+            sum += parseFloat(item.dataset.total);
+        });
+
+        totalEl.textContent = sum.toFixed(2);
+    }
+
+    document.querySelectorAll('.deleteBtn').forEach(btn => {
+        btn.addEventListener('click', () => {
+
+            if (!confirm('Remove this item from basket?')) return;
+
+            const basketItem = btn.closest('.basket-item');
+
+            basketItem.style.opacity = '0';
+            basketItem.style.transform = 'scale(0.95)';
+
+            setTimeout(() => {
+                basketItem.remove();
+                recalculateTotal(); 
+            }, 200);
+        });
+    });
+
+
+    if (buyBtn) {
+        buyBtn.addEventListener('click', () => {
+            alert("You have successfully completed your purchase.")
+
+              window.location.href = "checkout.php";
+        });
+    }
+
+});
